804ce7085754b5ea9fec6cc5f446a82f59774f37
Aidan Low <aidan.low98@gmail.com>
2019-03-19 12:39:12 -0400

    working api

--- popup.js
+++ popup.js
@@ -7,10 +7,52 @@ chrome.identity.getAuthToken({ 'interactive': true }, function(token) {
   })
 
   const queryParams = { headers };
+  const api_url = 'https://www.googleapis.com/calendar/v3'
 
-  fetch('https://www.googleapis.com/calendar/v3/calendars/primary/events', queryParams)
+  // Get the primary calendar
+  var url = new URL(api_url + '/users/me/calendarList');
+  fetch(url, queryParams)
   .then((response) => response.json()) // Transform the data into json
   .then(function(data) {
     console.log(data);
+    var calendarList = data["items"];
+    var primaryCalendar;
+    for (var i = 0; i < calendarList.length; i ++) {
+      if (calendarList[i]['primary'] == true) {
+        primaryCalendar = calendarList[i];
+        // document.getElementById("calendarList").innerHTML += '<div class="col-12"><b>' + calendarList[i]['summary'] + '</b></div>'
+      }
+      else {
+        // document.getElementById("calendarList").innerHTML += '<div class="col-12">' + calendarList[i]['summary'] + '</div>'
+      }
+    }
+    console.log(primaryCalendar);
+
+    // Get single calendar
+    var url = new URL(api_url + '/calendars/' + primaryCalendar['id']);
+    fetch(url, queryParams)
+    .then((response) => response.json()) // Transform the data into json
+    .then(function(data) {
+      console.log(data);
+      // document.getElementById("primaryCalendar").innerHTML += '<div class="col-12"><h3>' + primaryCalendar['summary'] + '</h3><p>' + primaryCalendar['description'] + '</p></div>'
+
+      // Get all events from primaryCalendar
+      var today = new Date();
+      console.log(today);
+
+      var url = new URL(api_url + '/calendars/' + primaryCalendar['id'] + '/events'),
+          params = {
+            singleEvents: true,
+            orderby: "startTime",
+            maxResults: 2500,
+            timeMax: today.toISOString()
+          }
+      Object.keys(params).forEach(key => url.searchParams.append(key, params[key]))
+      fetch(url, queryParams)
+      .then((response) => response.json()) // Transform the data into json
+      .then(function(data) {
+        console.log(data);
+      })
+    })
   })
 })
